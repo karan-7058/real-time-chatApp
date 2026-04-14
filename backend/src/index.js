bf5d4fe283a45810ef1e9eeaa7d6d4d7f4ae55a5
karan-7058 <aghavkaran164@gmail.com>
2026-04-14 17:29:37 +0530

    create an entry point for backend app

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Parse JSON / form bodies so req.body is defined
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use("/api/auth" , authRoute);
+
+
+app.listen(PORT, ()=>{
+    console.log("server is running on port 3000");
+    connectDB();
+
+})
+
